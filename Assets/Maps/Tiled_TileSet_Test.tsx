87983f75292a8b8f9cd708fb6412f02c3d1ceac3
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-07-09 22:11:03 +0300

    Figuring out physics with 2d isometric game
    Just added a free playable character to test out how physics work in isometric game. it is a 2D plain so there might be no need for physics.

--- Assets/Maps/Tiled_TileSet_Test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Tiled_TileSet_Test" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../Maps/AsepriteTile_Test_PNG.png" width="32" height="32"/>
-</tileset>
